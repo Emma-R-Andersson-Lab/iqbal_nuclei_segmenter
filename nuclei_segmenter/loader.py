0541fc2ce70389aa349a81a328b0bf92d0a4d87a
acorbat <agustin.corbat@ki.se>
2023-08-10 17:37:25 +0200

    Add get nuclei_size_df function with parser

--- nuclei_segmenter/loader.py
+++ nuclei_segmenter/loader.py
@@ -1,5 +1,6 @@
 from czifile import CziFile
 import numpy as np
+import pandas as pd
 from tifffile import TiffFile
 
 
@@ -28,3 +29,70 @@ def get_edu_image(file):
 
 def get_labeled(label_filepath):
     return TiffFile(label_filepath).asarray()
+
+
+def parse_condition(text):
+    if 'wt' in text.lower():
+        return 'WT'
+    elif 'ndr' in text.lower():
+        return 'Ndr'
+    else:
+        raise ValueError
+
+
+def parse_cell_line(text):
+    return text.split(' ')[0][-4:]
+
+
+def parse_region(text):
+    if 'peri' in text.lower():
+        return 'Peripheral'
+    elif 'hilar' in text.lower():
+        return 'Hilar'
+    else:
+        raise ValueError
+
+
+def parse_sample_number(text):
+    return text.split('-')[-1]
+
+
+def parse_igf1(text):
+    if 'igf1' in text.lower():
+        return 'IGF1'
+    elif 'ctrl' in text.lower():
+        return 'Control'
+    else:
+        raise ValueError
+
+
+def get_nuclei_size_df(filepath):
+    nuclei_size = pd.read_excel(filepath / 'nuclei_size.xlsx',
+                                         sheet_name=None)
+    dfs = []
+    for sample_name, sample_df in nuclei_size.items():
+        sample_df.drop(columns=['Unnamed: 0.1', 'Unnamed: 0'], inplace=True)
+        sample_df['sample_name'] = sample_name
+        dfs.append(sample_df)
+
+    nuclei_size = pd.concat(dfs, ignore_index=True)
+
+    nuclei_size['condition'] = nuclei_size.sample_name.apply(parse_condition)
+    nuclei_size['cell_line'] = nuclei_size.sample_name.apply(parse_cell_line)
+    nuclei_size['region'] = nuclei_size.sample_name.apply(parse_region)
+    nuclei_size['sample_number'] = nuclei_size.sample_name.apply(parse_sample_number)
+
+    nuclei_size = nuclei_size.astype({'label': int, 
+                                      'condition': 'category', 
+                                      'cell_line': 'category', 
+                                      'region': 'category', 
+                                      'sample_number': 'category', 
+                                    })
+
+    try:
+        nuclei_size['igf1'] = nuclei_size.sample_name.apply(parse_igf1)
+        nuclei_size = nuclei_size.astype({'igf1': 'category'})
+    except ValueError:
+        pass
+
+    return nuclei_size
